aa1f4c81ab29399ccab1440a07b82ba8c0e8f214
rafal.swiderski <rafal.swiderski@famoc.com>
2023-01-04 20:04:20 +0100

    Add log for checker and cron

--- checker.js
+++ checker.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const cron = require("node-cron");
 const dotenv = require("dotenv");
 dotenv.config();
 const client = require("twilio")(
@@ -6,22 +7,25 @@ const client = require("twilio")(
   process.env.AUTH_TOKEN
 );
 const users = JSON.parse(process.env.USERS);
-const now = new Date();
-const key =
-  now.getDate() + "." + (now.getMonth() + 1) + "." + now.getFullYear();
-const jsonLog = openJSONFile(process.env.JSON_LOG_PATH);
 
-let userWhichNotInLog = [];
 
-for (const [user, value] of Object.entries(users)) {
-  if (!jsonLog[key].includes(user)) {
-    userWhichNotInLog.push(user);
+cron.schedule("* 21 * * *", function () {
+  const now = new Date();
+  const key =
+    now.getDate() + "." + (now.getMonth() + 1) + "." + now.getFullYear();
+  log("I will try send sms");
+  const jsonLog = openJSONFile(process.env.JSON_LOG_PATH);
+  let userWhichNotInLog = [];
+
+  for (const [user, value] of Object.entries(users)) {
+    if (!(key in jsonLog) || !jsonLog[key].includes(user)) {
+      userWhichNotInLog.push(user);
+    }
   }
-}
-
-if (userWhichNotInLog.length !== 0) {
-  sendSms(userWhichNotInLog.join(","));
-}
+  if (userWhichNotInLog.length !== 0) {
+    sendSms(userWhichNotInLog.join(","));
+  }
+});
 
 function openJSONFile(filePath) {
   const jsonString = fs.readFileSync(filePath);
@@ -29,7 +33,6 @@ function openJSONFile(filePath) {
 }
 
 function sendSms(message) {
-  //todo:: sprobowac wywołać bład i zobaczyć, czy pokaze sie w logach i czy znów spróbuje wysłać
   client.messages
     .create({
       body:
@@ -39,6 +42,19 @@ function sendSms(message) {
       messagingServiceSid: process.env.MESSAGING_SERVICE_SID,
       to: "+48663270503",
     })
-    .then((message) => console.log(message.sid))
+    .then((message) => log(message.sid))
+    .catch((error) => {
+        log("Can not send sms. I will try again in 5 minutes")
+        log(error)
+      setTimeout(function () {
+        sendSms(message);
+      }, 300000);
+    })
     .done();
+}
+
+function log(message) {
+    const now = new Date();
+    const time = now.getDate() + "." + (now.getMonth() + 1) + "." + now.getFullYear() + ' ' + now.getHours() + ':' + now.getMinutes();
+    console.log(time+ ' - ' +message);
 }
