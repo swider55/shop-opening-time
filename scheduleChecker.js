f2fdbe6114a2a372cec4b060ae9a194e3defb1e4
rafal.swiderski <rafal.swiderski@famoc.com>
2023-02-23 10:30:03 +0100

    Add scheduled test sms

--- scheduleChecker.js
+++ scheduleChecker.js
@@ -8,8 +8,16 @@ const client = require("twilio")(
 );
 const users = JSON.parse(process.env.USERS);
 
+const OWNER_NUMBER ="+48518495816"
+
+const ADMIN_NUMBER ="+48663270503"
+
 cron.schedule("10 9 * * 1,2,3,4,5,6", function () {
     run();
+});
+
+cron.schedule('0 12 * * 0', () => {
+    sendSms('Test sms from shop-opening-time', ADMIN_NUMBER);
 });
 
 function run() {
@@ -18,7 +26,7 @@ function run() {
         now.getDate() + "." + (now.getMonth() + 1) + "." + now.getFullYear();
     log("Check jsonLog");
     if (!fs.existsSync(process.env.JSON_LOG_PATH)) {
-        sendSms('UWAGA! Nie ma pliku jsonLog. Skontaktuj sie z administratorm');
+        sendSms('UWAGA! Nie ma pliku jsonLog. Skontaktuj sie z administratorm', OWNER_NUMBER);
         return;
     }
 
@@ -34,7 +42,7 @@ function run() {
         log("I will send sms");
         sendSms("UWAGA! Te sklepy nie daly znac dzisiaj o sobie: " +
             userWhichNotInLog.join(",") +
-            ". Sprawdz co sie stalo");
+            ". Sprawdz co sie stalo", OWNER_NUMBER);
     }
 }
 
@@ -43,13 +51,13 @@ function openJSONFile(filePath) {
   return JSON.parse(jsonString);
 }
 
-function sendSms(message) {
+function sendSms(message, receiveNumber) {
   client.messages
     .create({
       body:
         message,
       messagingServiceSid: process.env.MESSAGING_SERVICE_SID,
-      to: "+48518495816",
+      to: receiveNumber,
     })
     .then((message) => log(message.sid))
     .catch((error) => {
